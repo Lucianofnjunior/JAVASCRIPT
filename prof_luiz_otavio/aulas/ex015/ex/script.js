316748793d410bac09c64890a3ae1e9033f0aa86
Lucianofnjunior <145075484+Lucianofnjunior@users.noreply.github.com>
2023-10-18 16:44:12 -0300

    Exercicio com math e number concluido

--- prof_luiz_otavio/aulas/ex015/ex/script.js
+++ prof_luiz_otavio/aulas/ex015/ex/script.js
@@ -1,6 +1,11 @@
-const numero = prompt('Digite um número:');
+const numero = Number(prompt('Digite um número:'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto')
 
 numeroTitulo.innerHTML = numero
-texto.innerHTML = `<p>Seu número - 2 é ${Number(numero) + 2}`
+texto.innerHTML = `<p>Raiz quadrada: ${numero **(1/2)} </p>
+<p>${numero} é inteiro: ${Number.isInteger(numero)} </p>
+<p>É NaN: ${isNaN(numero)} </p>
+<p>Arredondando para baixo: ${Math.floor(numero)} </p>
+<p>Arredondando para cima: ${Math.ceil(numero)} </p>
+<p>Com duas casas decimais: ${numero.toFixed(2)} </p>`
