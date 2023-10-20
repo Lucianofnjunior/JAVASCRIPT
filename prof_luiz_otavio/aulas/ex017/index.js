7753505477792344bc2639f3e88f7942ff966b38
Lucianofnjunior <145075484+Lucianofnjunior@users.noreply.github.com>
2023-10-20 12:31:31 -0300

    first noters about function

---
+++ prof_luiz_otavio/aulas/ex017/index.js
@@ -0,0 +1,14 @@
+//      FUNCTION
+
+/*function saudacao() {
+    console.log('Olá mundo!')
+}
+
+saudacao();*/
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(1, 2));
