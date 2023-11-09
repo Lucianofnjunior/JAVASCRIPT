24a0fe52b39f2c3949cba04652787f126763a5a6
Lucianofnjunior <145075484+Lucianofnjunior@users.noreply.github.com>
2023-11-09 20:05:11 -0300

    Exercicio finalizado - Array, function, methods and objects

--- prof_luiz_otavio/aulas/ex020/js/script.js
+++ prof_luiz_otavio/aulas/ex020/js/script.js
@@ -1,18 +1,46 @@
 function meuEscopo() {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    
+    const pessoas = [];
 
-   /*   Função mais arcaica
-   form.onsubmit = function (evento) {
-        evento.preventDefault();
-        alert(1)
-        console.log('Formulário enviado.')
-    };
-}
-*/
-//          FUNÇÃO MAIS ATUAL
     function recebeEventoForm (evento) {
+        evento.preventDefault()
 
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        const info = {
+            name: nome.value,
+            aftername: sobrenome.value,
+            weight: peso.value,
+            height: altura.value,
+    
+            fala() {
+                pessoas.shift()
+                pessoas.shift()
+                pessoas.shift()
+                pessoas.shift()
+                pessoas.push(this.name) 
+                pessoas.push(this.aftername)
+                pessoas.push(this.weight)
+                pessoas.push(this.height)
+                console.log(pessoas)
+            },
+        
+        };
+        info.fala();
+        
+        const resultado = document.querySelector('.resultado')
+        resultado.innerHTML = `${pessoas[0]}, ${pessoas[1]}, ${pessoas[2]}kg e ${pessoas[3]}m`
     }
+
+    
     form.addEventListener('submit', recebeEventoForm);
 }
+
+    
+
 meuEscopo();
