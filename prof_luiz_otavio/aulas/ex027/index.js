19d66c7152e708a339ab58b1364aca6686d1a2ca
Lucianofnjunior <145075484+Lucianofnjunior@users.noreply.github.com>
2024-01-26 14:51:34 -0300

    class operadores_ternarios

---
+++ prof_luiz_otavio/aulas/ex027/index.js
@@ -0,0 +1,15 @@
+// ? :  OPERADORES TERNÁRIOS
+// ? = if
+// : = else
+
+const pontuacaoUser = 1001;
+const nivelUser = pontuacaoUser >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUser)
+
+// OPERAÇÃO COM IF E ELSE
+//if (pontuacaoUser >= 1000) {
+//    console.log('Usuário VIP');
+//} else {
+//    console.log('Usuário normal');
+//}
+ 
